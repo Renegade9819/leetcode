568993ad62cd3cee113aef357b2a0ce72c084895
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-07-20 19:24:35 +0530

    Update Remove Duplicates from sorted array.py

--- Easy/Remove Duplicates from sorted array.py
+++ Easy/Remove Duplicates from sorted array.py
@@ -25,3 +25,25 @@ uptil the last unique element.
 In reality, the array after the calculation becomes [1, 2, 3, 4, 5, 4, 4, 5],
 which is not technically the correct answer.
 '''
+
+
+''' Here is another solution which actually removes the duplicate elements from the list and returns the length.
+    But it is very inefficient.
+    
+from collections import Counter
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if (len(nums) == 0):
+            return 0
+        
+        d = Counter(nums)
+        for num in nums:
+            ele = d[num]
+            if ele == 1:
+                continue
+            else:
+                for _ in range(ele-1):
+                    nums.remove(num)                
+        return len(nums)
+'''
