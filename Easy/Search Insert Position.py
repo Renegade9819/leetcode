7acb579c9c03899375beaa34e85673605e5c5eb0
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-07-23 19:47:51 +0530

    Update Search Insert Position.py

--- Easy/Search Insert Position.py
+++ Easy/Search Insert Position.py
@@ -1,8 +1,9 @@
+import bisect
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
         else:
-            nums.append(target)
-            nums = sorted(nums)
+            bisect.insort(nums, target)
             return nums.index(target)
