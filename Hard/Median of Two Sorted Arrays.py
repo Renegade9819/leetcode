7d515ce40d2c92b622ceb720deb2da3357a76dac
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-07-28 01:28:17 +0530

    Create Median of Two Sorted Arrays.py

---
+++ Hard/Median of Two Sorted Arrays.py
@@ -0,0 +1,7 @@
+# By using the in-built python statistics module
+
+import statistics
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        return statistics.median(nums1 + nums2)
