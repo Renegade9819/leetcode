68ea20e2840d367e1af52b6f955d16086e49c968
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-07-28 01:32:34 +0530

    Update Median of Two Sorted Arrays.py

--- Hard/Median of Two Sorted Arrays.py
+++ Hard/Median of Two Sorted Arrays.py
@@ -5,3 +5,15 @@ import statistics
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         return statistics.median(nums1 + nums2)
+
+    
+    
+''' Another simple solution -
+nums = sorted(nums1+nums2)
+if len(nums) % 2 == 0:
+    x = nums // 2
+    y = (nums // 2) - 1
+    return (x+y) / 2
+else:
+    return len(nums) // 2
+'''
