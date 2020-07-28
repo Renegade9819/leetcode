a9bcbe2452c2eafea3ef61fe15cfb5ec096c758f
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-07-29 03:34:35 +0530

    Update Shuffle String.py

--- Easy/Shuffle String.py
+++ Easy/Shuffle String.py
@@ -6,3 +6,12 @@ class Solution:
             res[indices[i]] = s[i]
         
         return ''.join(res)
+
+    
+'''
+Alternate Solution by ye15 - more efficient
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        return "".join(x for _, x in sorted(zip(indices, s)))
+'''
