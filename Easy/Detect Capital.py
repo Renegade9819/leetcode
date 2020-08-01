fe0485e44bd428f8d6d5c900911f63c8afaaabba
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-08-02 03:36:44 +0530

    Update Detect Capital.py

--- Easy/Detect Capital.py
+++ Easy/Detect Capital.py
@@ -2,9 +2,9 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         if word[0].isupper() == True and word[1:].islower() == True:
             return True
-        elif word[:].islower() == True:
+        elif word.islower() == True:
             return True
-        elif word[:].isupper() == True:
+        elif word.isupper() == True:
             return True
         else:
             return False
