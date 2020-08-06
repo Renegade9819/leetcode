b9d0033bd2dbd05cbf8a6ca4150a77783b7b5df4
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-08-07 01:27:52 +0530

    Update Number of Good Pairs.py

--- Easy/Number of Good Pairs.py
+++ Easy/Number of Good Pairs.py
@@ -5,3 +5,23 @@ class Solution:
         for k, v in count.items():
             res += v * (v - 1) // 2
         return res
+
+
+'''
+Another simple solution
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        
+        hashMap = {}
+        res = 0
+   
+        for number in nums:            
+            if number in hashMap:
+                res += hashMap[number]
+                hashMap[number] += 1
+            else:
+                hashMap[number] = 1
+                
+        return res
+'''
