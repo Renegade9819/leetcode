9461c0dd695faa32139340fa8bbed970136aecf9
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-08-08 15:30:05 +0530

    Create Add Binary.py

---
+++ Easy/Add Binary.py
@@ -0,0 +1,42 @@
+'''
+Solution - 1
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        maxLen = max(len(a), len(b))
+        
+        a = a.zfill(maxLen)
+        b = b.zfill(maxLen)
+        
+        result = ''
+        carry = 0
+        
+        i = maxLen - 1
+        
+        while(i >= 0):
+            add = int(a[i]) + int(b[i])
+            if add == 2:                                # 1 + 1
+                if carry == 0:
+                    carry = 1
+                    result = "%s%s" % (result, '0')
+                else:
+                    result = "%s%s" % (result, '1')
+            elif add == 1:                              # 1 + 0
+                if carry == 1:
+                    result = "%s%s" % (result, '0')
+                else:
+                    result = "%s%s" % (result, '1')
+            else:                                       # 0 + 0
+                if carry == 1:
+                    result = "%s%s" % (result, '1')
+                    carry = 0
+                else:
+                    result = "%s%s" % (result, '0')
+            
+            i -= 1
+            
+        if carry > 0:
+            result = "%s%s" % (result, '1')
+        
+        return result[::-1]
+'''
