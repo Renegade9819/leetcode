a6c1adcdf1a35ab4964639e475f447954239fdf1
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2020-08-26 17:06:30 +0530

    Create 172 Factorial Trailing Zeroes.py

---
+++ Easy/172 Factorial Trailing Zeroes.py
@@ -0,0 +1,22 @@
+'''
+
+    Take the number that you've been given the factorial of.
+    Divide by 5; if you get a decimal, truncate to a whole number.
+    Divide by 5^2 = 25; if you get a decimal, truncate to a whole number.
+    Divide by 5^3 = 125; if you get a decimal, truncate to a whole number.
+    Continue with ever-higher powers of 5, until your division results in a number less than 1. Once the division is less than 1, stop.
+    Sum all the whole numbers you got in your divisions. This is the number of trailing zeroes.
+
+'''
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        power = 1
+        count = 0
+        while(True):
+            num =  n // 5**power
+            power += 1
+            if num < 1:
+                return count
+            else:
+                count += num
