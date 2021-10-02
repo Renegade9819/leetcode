205e808b6f0cc9c4d0e5ebf6070dca5cf524d64c
Bhavin Bhanushali <bhavinpbhanushali@gmail.com>
2021-10-02 17:15:26 +0530

    Update Employees Earning more than their managers.sql

--- Database/Employees Earning more than their managers.sql
+++ Database/Employees Earning more than their managers.sql
@@ -22,10 +22,25 @@ Return the result table in any order.
 
 Solution 1 - 
 
-select e1.name as employee
-from employee e1
-where salary > (select e2.salary
-            from employee e2
-               where e1.ManagerId = e2.Id)
+select 
+   e1.name as employee
+from 
+   employee e1
+where 
+   salary > (select e2.salary
+             from employee e2
+             where e1.ManagerId = e2.Id);
+               
+               
+Solution 2 -
+
+SELECT 
+    e1.name AS Employee
+FROM 
+    employee e1 
+        JOIN employee e2
+            on e1.ManagerId = e2.Id AND
+            e1.salary > e2.salary;
+    
                
 
